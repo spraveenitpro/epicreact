f7ff3749a64114fca571f48b3c129989ac2207ef
Praveen <spraveenitpro@gmail.com>
2022-04-02 23:53:22 -0700

    Refreshed React hooks

--- react-hooks/src/exercise/01.js
+++ react-hooks/src/exercise/01.js
@@ -3,27 +3,47 @@
 
 import * as React from 'react'
 
-function Greeting() {
-  // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+function Greeting({initialName = ''}) {
+    const [name, setName] = React.useState(initialName)
 
-  function handleChange(event) {
-    // 🐨 update the name here based on event.target.value
-  }
+    function handleChange(event) {
+        // 🐨 update the name here based on event.target.value
+        setName(event.target.value)
+    }
 
-  return (
-    <div>
-      <form>
-        <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
-      </form>
-      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
-    </div>
-  )
+    return (
+        <div>
+            <form>
+                <label htmlFor="name">Name: </label>
+                <input onChange={handleChange} id="name" />
+            </form>
+            {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+        </div>
+    )
+}
+
+function Counter() {
+    const [count, setCount] = React.useState(0)
+
+    function handleClick() {
+        setCount(count + 1)
+    }
+
+    return (
+        <>
+            <button onClick={handleClick}>Click Me</button>
+            <p>{count ? count : 'Please click'}</p>
+        </>
+    )
 }
 
 function App() {
-  return <Greeting />
+    return (
+        <>
+            <Greeting initialName="Steve1" />
+            <Counter />
+        </>
+    )
 }
 
 export default App
--- react-hooks/src/exercise/02.js
+++ react-hooks/src/exercise/02.js
@@ -1,33 +1,38 @@
-// useEffect: persistent state
-// http://localhost:3000/isolated/exercise/02.js
-
 import * as React from 'react'
 
+function useLocalStorage(key, defaultValue) {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [state, setState] = React.useState(
+        // eslint-disable-next-line no-undef
+        () => window.localStorage.getItem(key) || defaultValue,
+    )
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    React.useEffect(() => {
+        window.localStorage.setItem(key, state)
+    }, [key, state])
+
+    return [state, setState]
+}
+
 function Greeting({initialName = ''}) {
-  // 🐨 initialize the state to the value from localStorage
-  // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(initialName)
-
-  // 🐨 Here's where you'll use `React.useEffect`.
-  // The callback should set the `name` in localStorage.
-  // 💰 window.localStorage.setItem('name', name)
-
-  function handleChange(event) {
-    setName(event.target.value)
-  }
-  return (
-    <div>
-      <form>
-        <label htmlFor="name">Name: </label>
-        <input value={name} onChange={handleChange} id="name" />
-      </form>
-      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
-    </div>
-  )
+    const [name, setName] = useLocalStorage(initialName)
+    function handleChange(event) {
+        setName(event.target.value)
+    }
+    return (
+        <div>
+            <form>
+                <label htmlFor="name">Name: </label>
+                <input value={name} onChange={handleChange} id="name" />
+            </form>
+            {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+        </div>
+    )
 }
 
 function App() {
-  return <Greeting />
+    return <Greeting />
 }
 
 export default App
