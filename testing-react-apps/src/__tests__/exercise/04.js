dfb8c6f542ce168186af134b386777c6cfbcbff6
Praveen <spraveenitpro@gmail.com>
2022-04-28 16:10:23 -0700

    Daily update

--- testing-react-apps/src/__tests__/exercise/04.js
+++ testing-react-apps/src/__tests__/exercise/04.js
@@ -5,17 +5,38 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
+import faker from '@faker-js/faker'
+import {build, fake} from '@jackfranklin/test-data-bot'
 
-test('submitting the form calls onSubmit with username and password', () => {
+const buildLoginForm = build({
+    fields: {
+        username: faker.internet.userName(),
+        password: faker.internet.password(),
+    },
+})
+
+test('submitting the form calls onSubmit with username and password', async () => {
+    const handleSubmit = jest.fn()
+    render(<Login onSubmit={handleSubmit} />)
+
+    const {username, password} = buildLoginForm()
+
+    await userEvent.type(screen.getByLabelText(/username/i), username)
+    await userEvent.type(screen.getByLabelText(/password/i), password)
+    await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+    //screen.debug()
+
+    expect(handleSubmit).toHaveBeenCalledWith({
+        username,
+        password,
+    })
+    expect(handleSubmit).toHaveBeenCalledTimes(1)
+
     // 🐨 create a variable called "submittedData" and a handleSubmit function that
     // accepts the data and assigns submittedData to the data that was submitted
     // 💰 if you need a hand, here's what the handleSubmit function should do:
     // const handleSubmit = data => (submittedData = data)
     //
-    const submittedData
-    const handleSubmit = data => {
-        submittedData = data
-    }
     // 🐨 render the login with your handleSubmit function as the onSubmit prop
     //
     // 🐨 get the username and password fields via `getByLabelText`
