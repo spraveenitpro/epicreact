cfa2ea67191edbed217c7381837ae4b660292337
Praveen <spraveenitpro@gmail.com>
2022-05-19 15:21:01 -0700

    Initiate adopt me project

---
+++ adopt-me/src/App.js
@@ -0,0 +1,30 @@
+const Pet = (props) => {
+    return React.createElement('div', {}, [
+        React.createElement('h1', {}, props.name),
+        React.createElement('h2', {}, props.animal),
+        React.createElement('h2', {}, props.breed),
+    ])
+}
+
+const App = () => {
+    return React.createElement('div', {}, [
+        React.createElement('h1', {}, 'Adopt Me!'),
+        React.createElement(Pet, {
+            name: 'luna',
+            animal: 'Dog',
+            breed: 'Havanese',
+        }),
+        React.createElement(Pet, {
+            name: 'Bunny',
+            animal: 'Dog',
+            breed: 'Siamese',
+        }),
+        React.createElement(Pet, {
+            name: 'jony',
+            animal: 'Dog',
+            breed: 'pomeranian',
+        }),
+    ])
+}
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'))
