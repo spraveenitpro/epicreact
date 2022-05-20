2ec1c4a4641a21abfc2b4b64d9d95b81ac1bcfac
Praveen <spraveenitpro@gmail.com>
2022-05-19 23:39:53 -0700

    Using JSX instead of React.createElement

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,31 +1,18 @@
-/* global React ReactDOM */
-const Pet = (props) => {
-    return React.createElement('div', {}, [
-        React.createElement('h1', {}, props.name),
-        React.createElement('h2', {}, props.animal),
-        React.createElement('h2', {}, props.breed),
-    ])
-}
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Pet from './Pet'
+
+
 
 const App = () => {
-    return React.createElement('div', {}, [
-        React.createElement('h1', {}, 'Adopt Me!'),
-        React.createElement(Pet, {
-            name: 'luna',
-            animal: 'Dog',
-            breed: 'Havanese',
-        }),
-        React.createElement(Pet, {
-            name: 'Bunny',
-            animal: 'Dog',
-            breed: 'Siamese',
-        }),
-        React.createElement(Pet, {
-            name: 'jony',
-            animal: 'Dog',
-            breed: 'pomeranian',
-        }),
-    ])
+    return (
+        <div>
+            <h1>Adopt Me!</h1>
+            <Pet name="Luna" animal="Dog" Breed="Havanese" />
+            <Pet name="Digo" animal="Dog" Breed="Havanese" />
+            <Pet name="Star" animal="Dog" Breed="Havanese" />
+        </div>
+    )
 }
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'))
+ReactDOM.render(<App/>, document.getElementById("root"))
