9077ada54b5a6f916c3fa760dc96e67050f3309e
Praveen <spraveenitpro@gmail.com>
2022-05-20 23:35:54 -0700

    Built search component

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Pet from './Pet'
-
-
+//import Pet from './Pet'
+import SearchParams from './SearchParams'
 
 const App = () => {
     return (
         <div>
             <h1>Adopt Me!</h1>
-            <Pet name="Luna" animal="Dog" Breed="Havanese" />
-            <Pet name="Digo" animal="Dog" Breed="Havanese" />
-            <Pet name="Star" animal="Dog" Breed="Havanese" />
+            {/*    <Pet name="Luna" animal="Dog" Breed="Havanese" />
+            <Pet name="Digo" animal="Dog" Breed="POmeranian" />
+            <Pet name="Star" animal="Dog" Breed="Havanese" /> */}
+            <SearchParams />
         </div>
     )
 }
 
-ReactDOM.render(<App/>, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById('root'))
---
+++ adopt-me/src/SearchParams.js
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+const SearchParams = () => {
+    //const location = 'Seattle WA'
+    const [location, setLocation] = useState('Seattle WA')
+    return (
+        <div className="search-params">
+            <form action="">
+                <label htmlFor="location">
+                    Location
+                    <input
+                        id="location"
+                        value={location}
+                        placeholder="Location"
+                    />
+                </label>
+                <button>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default SearchParams
